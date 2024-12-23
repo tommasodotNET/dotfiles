95109babc508e0f490c47ee057acc93d7e1cedc2
Tommaso Stocchi <tstocchi@microsoft.com>
2024-12-23 15:43:33 +0100

    adds kubectl/node cli

---
+++ node/install.sh
@@ -0,0 +1,5 @@
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+source ~/.zshrc
+# download and install Node.js (you may need to restart the terminal)
+nvm install 22
