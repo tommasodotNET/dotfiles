ed622e1f04351787ed779ae790d5485c51ccb3b6
Tommaso Stocchi <tstocchi@microsoft.com>
2024-12-23 15:01:36 +0100

    try/catch on go/dotnet install

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -1,8 +1,12 @@
 echo "Installing dotnet"
-sudo add-apt-repository ppa:dotnet/backports
+
+{ #try 
 sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-9.0
-echo "dotnet installed"
-
-echo "Trusting dotnet dev certs"
-dotnet dev-certs https --trust
+  echo "dotnet installed"
+  
+  echo "Trusting dotnet dev certs"
+  dotnet dev-certs https --trust
+} || { #catch
+  echo "dotnet install failed"
+}
--- go/install.sh
+++ go/install.sh
@@ -1,4 +1,8 @@
-echo "Installing go"
-wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
-rm go1.23.4.linux-amd64.tar.gz
+{ #try
+    echo "Installing go"
+    wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
+    sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
+    rm go1.23.4.linux-amd64.tar.gz
+} || { #catch
+    echo "go install failed"
+}
