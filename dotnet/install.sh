42d75d58ba92df08b8a4e5bb94509880c4ad2357
Tommaso Stocchi <tommasostocchi@outlook.com>
2025-08-31 20:01:58 +0200

    fixes order

--- dotnet/install.sh
+++ dotnet/install.sh
@@ -3,6 +3,8 @@ echo "Installing dotnet"
 { #try 
   curl https://builds.dotnet.microsoft.com/dotnet/Sdk/10.0.100-preview.7.25380.108/dotnet-sdk-10.0.100-preview.7.25380.108-linux-x64.tar.gz
   tar zxf dotnet-sdk-10.0.100-preview.7.25380.108-linux-x64.tar.gz -C /usr/local/share/dotnet
+
+  sudo ln -s /usr/local/share/dotnet/dotnet /usr/local/bin
   
   echo "Trusting dotnet dev certs"
   dotnet dev-certs https --trust
--- dotnet/path.zsh
+++ dotnet/path.zsh
@@ -1,4 +1,2 @@
 export DOTNET_ROOT=/usr/local/share/dotnet
 export PATH=$PATH:$DOTNET_ROOT
-
-sudo ln -s /usr/local/share/dotnet/dotnet /usr/local/bin
